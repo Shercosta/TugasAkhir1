8f72d1b8aca5f567ecca01e0fe4a7e5a88282b08
Shercosta <70560582+Shercosta@users.noreply.github.com>
2022-12-30 00:30:46 +0700

    node+python tutorial basics

---
+++ nodePythonApp/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { spawn } = require("child_process");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  let dataToSend;
+  let largeDataSet = [];
+  // spawn new child process to call the python script
+  const python = spawn("python", ["script3.py"]);
+
+  // collect data from script
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    //dataToSend =  data;
+    largeDataSet.push(data);
+  });
+
+  // in close event we are sure that stream is from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(largeDataSet.join(""));
+  });
+});
+
+app.get("/2", (req, res) => {
+  let dataToSend;
+
+  const python = spawn("python", [
+    "script2.py",
+    "Shercosta ",
+    "is learning Node + Python ",
+    "for Tugas Akhir praypray",
+  ]);
+
+  python.stdout.on("data", (data) => {
+    dataToSend = data;
+  });
+
+  python.on("close", (code) => {
+    res.send(dataToSend.toString());
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
---
+++ nodePythonApp/script2.py
@@ -0,0 +1,3 @@
+import sys
+print('#Hello from python#')
+print(sys.argv[1] + sys.argv[2] + sys.argv[3])
